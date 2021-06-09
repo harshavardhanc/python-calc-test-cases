73549d6ba1071e92f91902eda8fbc2393d41a4a3
harshavardhanc <harshavardhanc95@gmail.com>
2021-06-09 10:52:37 +0530

    adding test cases

--- test_calculator.py
+++ test_calculator.py
@@ -12,5 +12,8 @@ class TestCalculator(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(calculator.subtract(5, 1), 4)
 
+    def test_multiply(self):
+        self.assertEqual(calculator.multiply(5, 1), 5)
+
 if __name__ == '__main__':
     unittest.main()
